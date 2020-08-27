fe217b7a268157a86e15a2f6dc3b8d18845ecbb0
Alberto Zordan <zorberto@gmail.com>
2020-08-27 14:45:08 +0200

    Some additions...

--- js/script.js
+++ js/script.js
@@ -27,25 +27,31 @@ $(document).ready(function () {
         {
             'nome': 'Alberto',
             'cognome': 'Zordan',
-            'eta': 34
+            'eta': 34,
+            'sesso': 'm'
         },
         {
             'nome': 'Marco',
             'cognome': 'Colussi',
-            'eta': 32
+            'eta': 32,
+            'sesso': 'm'
+
         },
         {
             'nome': 'Eleonora',
             'cognome': 'Cassani',
-            'eta': 29
+            'eta': 29,
+            'sesso': 'f'
         }
     ];
 
     // Stampa delle proprietà dei vari oggetti combinando i cicli 'for in' e 'for'
     for (var i = 0; i < studenti.length; i++) {
-        for (key in studenti[i]) {
-            console.log(`${key}: ${studenti[i][key]}`);
-        }
+        // for (key in studenti[i]) {
+        //     console.log(`${key}: ${studenti[i][key]}`);
+        // }
+        console.log(`${studenti[i].nome} ${studenti[i].cognome}`);
+        // console.log(Object.keys(studenti[i])[0]); così ottengo 'nome'. Object.keys è il metodo sull'oggetto built-in 'Object' che recupera le chiavi delle proprietà
     }
 
     // 3. Inserimento nell'array di un nuovo oggetto studente con le proprietà già viste (tramite prompt)
@@ -57,30 +63,29 @@ $(document).ready(function () {
     //     'nome': userName,
     //     'cognome': userSurname,
     //     'eta': userAge
-    // });
+    // });    
+    // console.log(studenti);
 
-    // for (var i = 0; i < studenti.length; i++) {
-    //     for (key in studenti[i]) {
-    //         console.log(`${key}: ${studenti[i][key]}`);
-    //     }
-    // }
+    // oppure faccio var newStudent = {'nome': prompt('Inserisci il nome'), 'cognome': prompt('Inserisci il cognome'), 'eta': parseInt(prompt('Inserisci l\'età'))} e poi studenti.push(newStudent)    
 
     // 3. Inserimento nell'array di un nuovo oggetto studente con le proprietà già viste
     // Reset dei campi di input
-    $('input').val('');
+    $('input, select').val('');
 
     $('#enter').on('click', function () {
         // Acquisizione dati inseriti
         var userName = $('#name').val();
         var userSurname = $('#surname').val();
         var userAge = $('#age').val();
+        var userSex = $('#sex').val();
 
         // Controllo sugli input e push nell'array del nuovo oggetto 'studente'
-        if (userName && userSurname && userAge) {
+        if (userName && userSurname && userAge && userSex) {
             studenti.push({
                 'nome': userName,
                 'cognome': userSurname,
-                'eta': userAge
+                'eta': userAge,
+                'sesso': userSex
             });
 
             // Utilizzo del template di Handlebars per appendere l'array di oggetti
